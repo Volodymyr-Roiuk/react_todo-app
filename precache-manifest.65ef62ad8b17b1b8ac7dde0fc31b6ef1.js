83b71dda90b4a5b5c4da15f9827e017f3d264577
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-31 20:34:28 +0300

    Updates

---
+++ precache-manifest.65ef62ad8b17b1b8ac7dde0fc31b6ef1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1774f7bd3b6009bef1483453af82d7b",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "5d9925de059f7690a1de",
+    "url": "/react_todo-app/static/css/main.706bb5a7.chunk.css"
+  },
+  {
+    "revision": "03001256bdc820cfdad8",
+    "url": "/react_todo-app/static/js/2.146b4175.chunk.js"
+  },
+  {
+    "revision": "5d9925de059f7690a1de",
+    "url": "/react_todo-app/static/js/main.1612e023.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
